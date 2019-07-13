a140c46f0ff518aac94a88dc7294ad469e75d473
Kunhua Zhang(张坤华) <zhangkunhua@le.com>
2019-07-13 12:59:08 +0800

    init

---
+++ src/pages/Home.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="bg">
+      <home-swiper :list="swiperList"></home-swiper>
+    </div>
+</template>
+
+<script>
+import HomeSwiper from './Swiper'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeSwiper
+  },
+  data () {
+    return {
+      swiperList: [{
+        "id": "0001",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "http://img1.qunarzz.com/piao/fusion/1810/5d/b63d846958a4702.jpg_750x200_8129cfc3.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      }, {
+        "id": "0002",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      },{
+        "id": "0003",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "http://img1.qunarzz.com/piao/fusion/1810/29/45722c73b5b66902.jpg_750x200_231ae61f.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      }, {
+        "id": "0004",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "https://imgs.qunarzz.com/p/tts6/1804/76/e21a1f848db63302.jpg_r_390x260x90_c4873f7a.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      },
+      {
+        "id": "0005",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "https://imgs.qunarzz.com/p/tts5/1808/81/915df9066af34302.jpg_r_390x260x90_7975908a.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      },
+      {
+        "id": "0006",
+        "file": "/xxx/xxx/xxx.mp4", // 模板视频文件
+        "duration": 30170, // 时长，单位毫秒
+        "title": "标题",
+        "thumb": "http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg", // 展示缩略图
+        "description": "描述",
+        "url": "http://xxx/xxx/xxx/xxx.mp4" // 模板预览地址
+      }]
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      //  用axios请求ajax数据
+      // axios.get('/api/index.json?city=' + this.city)
+      axios.get('/static/mock/index.json?city=' + this.city)
+        .then((result) => {
+          this.getHomeInfoSucc(result)
+        }).catch((err) => {
+          console.log(`请求数据出错: ${err}`)
+        })
+      // 用fetch请求ajax数据
+      // fetch('/api/index.json')
+      //   .then(data => data.json())
+      //   .then(res => this.getHomeInfoSucc(res))
+      //   .catch(err => console.log(`请求数据出错：${err}`))
+    },
+    getHomeInfoSucc (res) {
+      //  使用axios方法传进来的res数据
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        // this.city = data.city  这个不用了  用vuex实现数据共享
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+      // 使用fetch方法传进来的res数据
+      // if (res.ret && res.data) {
+      //   const data = res.data
+      //   this.city = data.city
+      //   this.swiperList = data.swiperList
+      //   this.iconList = data.iconList
+      //   this.recommendList = data.recommendList
+      //   this.weekendList = data.weekendList
+      // }
+    }
+  },
+  mounted () {
+    // this.getHomeInfo()
+  }
+}
+</script>
+
+<style lang="stylus" >
+  body {
+    width: 19.2rpx;
+    height: 100%;
+    display: flex;
+    border: 1px solid black
+    justify-content: center;
+    // background: url("~@/assets/bg.png") no-repeat center center;
+  }
+
+</style>
---
+++ src/pages/Swiper.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+    <!-- slides -->
+    <swiper-slide v-for="item in list" :key="item.id">
+      <img class="swiper-img" :src="item.thumb">
+    </swiper-slide>
+    <!-- <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>I'm Slide 4</swiper-slide>
+    <swiper-slide>I'm Slide 5</swiper-slide>
+    <swiper-slide>I'm Slide 6</swiper-slide>
+    <swiper-slide>I'm Slide 7</swiper-slide> -->
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div> <!--这个是拿来做按钮区的 -->
+    <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>  左右箭头 这里不需要-->
+    <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> 显示滚动条的 这不需要 -->
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        watchSlidesProgress: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        loop: true,
+        loopedSlides: 5,
+        // autoplay: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type : 'fraction'
+        },
+      }
+    }
+  },
+  mounted () {
+    console.log(this.list)
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    margin-top 2.7rem
+    width 9.22rem
+    // height 5.77rem
+    border 1px solid red
+    .swiper-img
+      width 100%
+      height 5.77rem
+</style>
